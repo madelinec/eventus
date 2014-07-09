e39c2bc029832d0ad6541b9711cb843e8be07b0b
Madeline Chapin <mcchapin@bu.edu>
2014-07-08 21:59:51 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eventus::Application.config.secret_key_base = '769ade7239862d46cee7555a9126e5ff7375f2b9432c1d40e46e9c227e85cce1b1f320ba13d832d6e36b308e7a9815f23f9fca6ffcf3d05a5a409af7463611d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Eventus::Application.config.session_store :cookie_store, key: '_eventus_session'
