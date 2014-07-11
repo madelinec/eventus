fee41243415be05da2842fadbc5615ac35696206
max <msteing@gmail.com>
2014-07-11 18:16:56 -0400

    add feed_entry model and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Eventus::Application.routes.draw do
+
+  root 'welcome#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,8 +57,5 @@ Eventus::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  
-  root 'welcome#index'
-  
   
 end
