8c4c4a871726dd895545f5579e1ded5403e07058
Madeline Chapin <mcchapin@bu.edu>
2014-07-16 19:39:38 -0400

    event types

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,23 +1,38 @@
 class WelcomeController < ApplicationController
 
   def index
-  	@entries = FeedEntry.all(:limit => 100, :order => "published asc")
+  	@entries = FeedEntry.all(:limit => 5, :order => "published asc")
   	@event_details = Hash.new
+  	@event_type = Hash.new
   	@locations = Array.new
   	@event_summaries = Hash.new
-
+  	@event_links = Hash.new
+    
+    type_regex = /Arts & Crafts|Attractions & Museums|Author Talk|Book Group Book Sale|Community Meeting|Computers\/Technology|
+    Concerts \/ Live Music|Early Literacy|Environmental Events|ESOL|Exhibition|Farmer's Market|Film|Food & Dining|
+    Games\/Gaming|Health \/ Fitness|Historical|Holiday Celebration|Homework Help|Meeting \/ Hearing|Nature|Non-Profit Fundraiser|
+    Parades & Festivals|Park Event|Performing Arts|Public Meeting \/ Hearing|Social Networking|Sports|Story Time|Talks & Lectures|
+    Theatre|Tours|Visual Arts|Volunteer Opportunity|Walks & Races|Workshops & Classes/
   	date_regex = /(Monday|Tuesday|Wednesday|Thursday|Friday), (January|February|March|April|May|June|July|August) (\d){1,2}, (\d){4}/
   	address_regex = /<br \/>(\d)+ ((\w)+ )+(\w)+<br \/>(\w)+(,)? MA (\d){5,}/
 
   	@entries.each do |entry| 
   		summary = entry.summary
   		title = entry.title
+  		link = entry.link
 
   		@event_summaries[title] = summary
+  		@event_links[title] = link
 
   		# match returns MatchData or nil
+  		type = type_regex.match(summary)
   		date = date_regex.match(summary)
   		html_address = address_regex.match(summary)
+  		
+  		unless type.nil?
+  		  type = type[0]
+  		  @event_type[entry.title] = type
+  		end
 
   		unless date.nil?
   			date = date[0]
